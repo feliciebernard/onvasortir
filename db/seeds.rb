66f87e42130263f1433899e6413b907acf1bb6e1
feliciebernard <felicie.bernard@outlook.fr>
2021-05-17 19:50:24 +0200

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'faker'
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     description: Faker::GreekPhilosophers.quote,
-    email: Faker::Name.first_name+'.'+Faker::Name.last_name+"@yopmail.com")
+    email: Faker::Name.first_name+"@yopmail.com")
 end
 
 tp User.all
