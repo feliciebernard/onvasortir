5f0210714943f76bb9e0e3268a79954d7aa6a38a
feliciebernard <felicie.bernard@outlook.fr>
2021-05-19 15:09:01 +0200

    ajout détails

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   after_create :welcome_send
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'events#index',  as: 'root'
   get 'static_pages/secret'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :attendances
   resources :events
   resources :users
